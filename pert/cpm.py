4d23478d5f6788adab503fda5cf58aaba9b19ef0
kornkawin <k.bijayayothin@gmail.com>
2025-03-08 17:46:01 +0700

    [UPD] refactor code and add comment

--- pert/cpm.py
+++ pert/cpm.py
@@ -1,75 +1,85 @@
-# Data structures
-nodes = [
-    [1, 0, 0],
-    [2, 0, 0],
-    [3, 0, 0],
-    [4, 0, 0],
-    [5, 0, 0],
-    [6, 0, 0],
-    [7, 0, 0],
-    [8, 0, 0],
-]
-
-edges = [
-    [1, 2, 12],
-    [2, 3, 8],
-    [2, 4, 4],
-    [2, 5, 3],
-    [3, 6, 12],
-    [4, 6, 18],
-    [4, 5, 5],
-    [5, 7, 8],
-    [6, 7, 4],
-    [7, 8, 6],
-]
-
-def calculate_earliest_times(nodes, edges):
-    """Forward pass: calculate earliest times (ET)"""
-    for edge in edges:
-        dest_node = edge[1]
+"""
+    Program to calculate the critical path of a project using the CPM method.
+"""
+def calculate_earliest_times(_nodes, _edges):
+    """Forward pass: calculate the earliest times (ET)"""
+    for e in _edges:
+        dest_node = e[1]
         # Find all edges leading to this destination and get max time
-        candidates = [nodes[e[0]-1][1] + e[2] for e in edges if e[1] == dest_node]
-        nodes[dest_node-1][1] = max(candidates)
-    return nodes
+        candidates = [_nodes[e[0]-1][1] + e[2] for e in _edges if e[1] == dest_node]
+        _nodes[dest_node-1][1] = max(candidates)
+    return _nodes
 
-def calculate_latest_times(nodes, edges):
-    """Backward pass: calculate latest times (LT)"""
-    for edge in reversed(edges):
-        src_node = edge[0]
+def calculate_latest_times(_nodes, _edges):
+    """Backward pass: calculate the latest times (LT)"""
+    for e in reversed(_edges):
+        src_node = e[0]
         # Find all edges starting from this node and get min time
-        candidates = [nodes[e[1]-1][2] - e[2] for e in edges if e[0] == src_node]
-        nodes[src_node-1][2] = min(candidates)
-    return nodes
+        candidates = [_nodes[e[1]-1][2] - e[2] for e in _edges if e[0] == src_node]
+        _nodes[src_node-1][2] = min(candidates)
+    return _nodes
 
-def find_critical_path(nodes):
+def find_critical_path(_nodes):
     """Find nodes where ET equals LT (critical path)"""
-    return [node[0] for node in nodes if node[1] == node[2]]
+    return [node[0] for node in _nodes if node[1] == node[2]]
 
-# Calculate earliest times (ET)
-nodes = calculate_earliest_times(nodes, edges)
+if __name__ == '__main__':
+    # Data structures
+    # Index 0: Node ID (from 1 to 8) - identifies each milestone or event in the project network
+    # Index 1: Earliest Time (ET) - initially set to 0, will be calculated during the forward pass algorithm
+    # Index 2: Latest Time (LT) - initially set to 0, will be calculated during the backward pass algorithm
+    nodes = [
+        [1, 0, 0],
+        [2, 0, 0],
+        [3, 0, 0],
+        [4, 0, 0],
+        [5, 0, 0],
+        [6, 0, 0],
+        [7, 0, 0],
+        [8, 0, 0],
+    ]
 
-# Set latest time (LT) of last node equal to its earliest time
-nodes[-1][2] = nodes[-1][1]
-
-# Calculate latest times (LT)
-nodes = calculate_latest_times(nodes, edges)
-
-print("Data Node with ET and LT")
-print(nodes)
-
-# Find critical path
-critical_path = find_critical_path(nodes)
-print("\n\nCritical Path")
-print(critical_path)
-
-# Calculate critical time
-critical_time = 0
-for i in range(len(critical_path) - 1):
-    current, next_node = critical_path[i], critical_path[i + 1]
-    for edge in edges:
-        if edge[0] == current and edge[1] == next_node:
-            critical_time += edge[2]
-            break
-
-print("\n\nCritical Time")
-print(critical_time)
+    # Index 0: Source node ID - the starting node of an activity
+    # Index 1: Destination node ID - the ending node of an activity
+    # Index 2: Duration - time required to complete the activity (in consistent units)
+    edges = [
+        [1, 2, 12],
+        [2, 3, 8],
+        [2, 4, 4],
+        [2, 5, 3],
+        [3, 6, 12],
+        [4, 6, 18],
+        [4, 5, 5],
+        [5, 7, 8],
+        [6, 7, 4],
+        [7, 8, 6],
+    ]
+    
+    # Calculate earliest times (ET)
+    nodes = calculate_earliest_times(nodes, edges)
+    
+    # Set latest time (LT) of last node equal to its earliest time
+    nodes[-1][2] = nodes[-1][1]
+    
+    # Calculate latest times (LT)
+    nodes = calculate_latest_times(nodes, edges)
+    
+    print("Data Node with ET and LT")
+    print(nodes)
+    
+    # Find critical path
+    critical_path = find_critical_path(nodes)
+    print("\n\nCritical Path")
+    print(critical_path)
+    
+    # Calculate critical time
+    critical_time = 0
+    for i in range(len(critical_path) - 1):
+        current, next_node = critical_path[i], critical_path[i + 1]
+        for edge in edges:
+            if edge[0] == current and edge[1] == next_node:
+                critical_time += edge[2]
+                break
+    
+    print("\n\nCritical Time")
+    print(critical_time)
--- pert/pert.py
+++ pert/pert.py
@@ -1,3 +1,6 @@
+"""
+    Program to calculate the critical path of a project using the PERT method.
+"""
 def calculate_t(a, m, b):
     """Calculate expected time using PERT formula"""
     return (a + 4 * m + b) / 6
@@ -28,70 +31,91 @@ def find_critical_path(data_node):
     """Find nodes where earliest time equals latest time (critical path)"""
     return [node[0] for node in data_node if node[1] == node[2]]
 
-# Constants for indexing
-IDX_THIS_NODE = 0
-IDX_DEST_NODE = 1
-IDX_TIME = 5
-IDX_VARIANCE = 6
 
-# Data
-data_node = [
-    [1, 0, 0],
-    [2, 0, 0],
-    [3, 0, 0],
-    [4, 0, 0],
-    [5, 0, 0],
-]
-
-data_relation = [
-    [1, 2, 5, 8, 17, 0, 0],
-    [1, 3, 7, 10, 13, 0, 0],
-    [2, 3, 3, 5, 7, 0, 0],
-    [2, 4, 1, 3, 5, 0, 0],
-    [3, 4, 4, 6, 8, 0, 0],
-    [3, 5, 3, 3, 3, 0, 0],
-    [4, 5, 3, 4, 5, 0, 0],
-]
-
-# Main execution
-# Calculate expected time and variance for each activity
-for rel in data_relation:
-    rel[IDX_TIME] = calculate_t(rel[2], rel[3], rel[4])
-    rel[IDX_VARIANCE] = calculate_variance(rel[2], rel[4])
-
-# Forward pass
-data_node = calculate_et(data_node, data_relation)
-
-# Set latest time of last node equal to its earliest time
-data_node[-1][2] = data_node[-1][1]
-
-# Backward pass
-data_node = calculate_el(data_node, data_relation)
-
-print("Data Node with ET and EL")
-print(data_node)
-
-# Find critical path
-critical_path = find_critical_path(data_node)
-print("\n\nCritical Path")
-print(critical_path)
-
-# Calculate critical time and total variance
-critical_time = 0
-sum_variance = 0
-
-# Find edges between consecutive critical path nodes
-for i in range(len(critical_path) - 1):
-    current = critical_path[i]
-    next_node = critical_path[i + 1]
+if __name__ == '__main__':
+    # Constants for indexing
+    # IDX_THIS_NODE: References the source node of an activity 
+    # (index 0 in each relation)
+    IDX_THIS_NODE = 0
+    # References the destination node of an activity 
+    # (index 1 in each relation)
+    IDX_DEST_NODE = 1
+    # References the expected time of an activity
+    # (index 5 in each relation)
+    IDX_TIME = 5
+    # References the variance of an activity
+    # (index 6 in each relation)
+    IDX_VARIANCE = 6
+    
+    # Data
+    # index 0: node ID
+    # index 1: earliest time (ET), initially set 0 and updated during forward pass
+    # index 2: latest time (LT), initially set 0 and updated during backward pass
+    data_node = [
+        [1, 0, 0],
+        [2, 0, 0],
+        [3, 0, 0],
+        [4, 0, 0],
+        [5, 0, 0],
+    ]
+    
+    # Data relation
+    # index 0: this node
+    # index 1: destination node
+    # index 2: optimistic time
+    # index 3: most likely time
+    # index 4: pessimistic time
+    # index 5: expected time
+    # index 6: variance
+    data_relation = [
+        [1, 2, 5, 8, 17, 0, 0],
+        [1, 3, 7, 10, 13, 0, 0],
+        [2, 3, 3, 5, 7, 0, 0],
+        [2, 4, 1, 3, 5, 0, 0],
+        [3, 4, 4, 6, 8, 0, 0],
+        [3, 5, 3, 3, 3, 0, 0],
+        [4, 5, 3, 4, 5, 0, 0],
+    ]
+    
+    # Main execution
+    # Calculate expected time and variance for each activity
     for rel in data_relation:
-        if rel[IDX_THIS_NODE] == current and rel[IDX_DEST_NODE] == next_node:
-            critical_time += rel[IDX_TIME]
-            sum_variance += rel[IDX_VARIANCE]
-            break
-
-print("\n\nCritical Time")
-print(critical_time)
-
-print("\n\nVariance")
-print(sum_variance)
+        rel[IDX_TIME] = calculate_t(rel[2], rel[3], rel[4])
+        rel[IDX_VARIANCE] = calculate_variance(rel[2], rel[4])
+    
+    # Forward pass
+    data_node = calculate_et(data_node, data_relation)
+    
+    # Set latest time of last node equal to its earliest time
+    data_node[-1][2] = data_node[-1][1]
+    
+    # Backward pass
+    data_node = calculate_el(data_node, data_relation)
+    
+    print("Data Node with ET and EL")
+    print(data_node)
+    
+    # Find critical path
+    critical_path = find_critical_path(data_node)
+    print("\n\nCritical Path")
+    print(critical_path)
+    
+    # Calculate critical time and total variance
+    critical_time = 0
+    sum_variance = 0
+    
+    # Find edges between consecutive critical path nodes
+    for i in range(len(critical_path) - 1):
+        current = critical_path[i]
+        next_node = critical_path[i + 1]
+        for rel in data_relation:
+            if rel[IDX_THIS_NODE] == current and rel[IDX_DEST_NODE] == next_node:
+                critical_time += rel[IDX_TIME]
+                sum_variance += rel[IDX_VARIANCE]
+                break
+    
+    print("\n\nCritical Time")
+    print(critical_time)
+    
+    print("\n\nVariance")
+    print(sum_variance)
