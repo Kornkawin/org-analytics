570f2636173937c946ec5a6182fb5cb248220b6d
kornkawin <k.bijayayothin@gmail.com>
2025-03-08 20:58:14 +0700

    [UPD] fixed bugs

--- schedule/cpm.py
+++ schedule/cpm.py
@@ -8,7 +8,7 @@ def calculate_earliest_times(_nodes, _edges):
         # Find all edges leading to this destination and get max time
         candidates = [_nodes[e[0]-1][1] + e[2] for e in _edges if e[1] == dest_node]
         _nodes[dest_node-1][1] = max(candidates)
-    return _nodes
+    return _nodes[:-1]
 
 def calculate_latest_times(_nodes, _edges):
     """Backward pass: calculate the latest times (LT)"""
@@ -17,7 +17,7 @@ def calculate_latest_times(_nodes, _edges):
         # Find all edges starting from this node and get min time
         candidates = [_nodes[e[1]-1][2] - e[2] for e in _edges if e[0] == src_node]
         _nodes[src_node-1][2] = min(candidates)
-    return _nodes
+    return _nodes[:-1]
 
 def find_critical_path(_nodes):
     """Find nodes where ET equals LT (critical path)"""
@@ -25,46 +25,53 @@ def find_critical_path(_nodes):
 
 if __name__ == '__main__':
     # Data structures
-    # Index 0: Node ID (from 1 to 8) - identifies each milestone or event in the project network
-    # Index 1: Earliest Time (ET) - initially set to 0, will be calculated during the forward pass algorithm
-    # Index 2: Latest Time (LT) - initially set to 0, will be calculated during the backward pass algorithm
+    # Index 0: Task ID - identifies each milestone or event in the project network
+    # Index 1: Earliest Finish (EF) - initially set to 0, will be calculated during the forward pass algorithm
+    # Index 2: Latest Finish (LF) - initially set to 0, will be calculated during the backward pass algorithm
     nodes = [
-        [1, 0, 0],
-        [2, 0, 0],
-        [3, 0, 0],
-        [4, 0, 0],
-        [5, 0, 0],
-        [6, 0, 0],
-        [7, 0, 0],
-        [8, 0, 0],
+        [0, 0, 0],  #START
+        [1, 0, 0],  #A
+        [2, 0, 0],  #B
+        [3, 0, 0],  #C
+        [4, 0, 0],  #D
+        [5, 0, 0],  #E
+        [6, 0, 0],  #F
+        [7, 0, 0],  #G
+        [8, 0, 0],  #H
+        [9, 0, 0],  #I
+        [10, 0, 0], #J
+        [11, 0, 0]  #END
     ]
 
     # Index 0: Source node ID - the starting node of an activity
     # Index 1: Destination node ID - the ending node of an activity
-    # Index 2: Duration - time required to complete the activity (in consistent units)
+    # Index 2: Actual Time - time required to complete the activity (in consistent units)
     edges = [
-        [1, 2, 12],
-        [2, 3, 8],
-        [2, 4, 4],
-        [2, 5, 3],
-        [3, 6, 12],
-        [4, 6, 18],
-        [4, 5, 5],
-        [5, 7, 8],
+        [0, 1, 3],      # START->A
+        [0, 2, 5],      # START->B
+        [1, 4, 4],
+        [2, 3, 3],
+        [2, 9, 5],
+        [3, 4, 4],
+        [3, 6, 2],
+        [4, 5, 8],
         [6, 7, 4],
-        [7, 8, 6],
+        [6, 8, 2],
+        [5, 10, 3],
+        [7, 10, 3],
+        [8, 10, 3],
+        [9, 11, 0],     # I->END
+        [10, 11, 0],    # J->END
     ]
     
     # Calculate earliest times (ET)
     nodes = calculate_earliest_times(nodes, edges)
-    
     # Set latest time (LT) of last node equal to its earliest time
     nodes[-1][2] = nodes[-1][1]
-    
     # Calculate latest times (LT)
     nodes = calculate_latest_times(nodes, edges)
-    
     print("Data Node with ET and LT")
+    
     print(nodes)
     
     # Find critical path
